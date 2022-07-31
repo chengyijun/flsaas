a62a55b270aad739bfcdadd0816dfb98b9a78f48
abel <cyjmmy@qq.com>
2022-07-31 21:53:23 +0800

    患者信息-最近

---
+++ src/components/PatientManage/Recent.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+        <div class="top">
+            <!--            上部 内容-->
+            <div class="left">
+                <div class="left_1">
+
+                </div>
+                <div class="left_2">
+                    <div>孙乐文</div>
+                    <div>FL000001</div>
+                </div>
+            </div>
+            <div class="right">
+                <span>初诊医生：张一航</span>
+                <span>|</span>
+                <span>联系号码：18356202563</span>
+                <span>|</span>
+                <span>初诊时间：2022-03-26</span>
+                <span>|</span>
+                <span>欠费数目：1600.00元</span>
+            </div>
+        </div>
+
+        <div class="main">
+            <!--            主体 内容-->
+            <el-tabs type="border-card">
+
+                <el-tab-pane label="患者信息">患者信息</el-tab-pane>
+                <el-tab-pane label="知情书">知情书</el-tab-pane>
+                <el-tab-pane label="就诊信息">就诊信息</el-tab-pane>
+                <el-tab-pane label="处置信息">处置信息</el-tab-pane>
+                <el-tab-pane label="收费管理">收费管理</el-tab-pane>
+                <el-tab-pane label="电子病例">电子病例</el-tab-pane>
+                <el-tab-pane label="影像中心">影像中心</el-tab-pane>
+                <el-tab-pane label="随访">随访</el-tab-pane>
+                <el-tab-pane label="套餐卡券">套餐卡券</el-tab-pane>
+            </el-tabs>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Recent"
+    }
+</script>
+
+<style scoped>
+    .top {
+        display: flex;
+
+        height: 100px;
+        box-shadow: 0px 3px 6px 1px rgba(167, 167, 167, 0.1600);
+    }
+
+
+    .left {
+        display: flex;
+        position: relative;
+        top: 10px;
+    }
+
+    .left_1 {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        /*background-color: red;*/
+        background: url("../../assets/images/avatar.jpeg") no-repeat;
+        background-size: cover;
+    }
+
+    .left_2 {
+        margin-left: 14px;
+    }
+
+    .left_2 div:nth-child(1) {
+        margin-top: 10px;
+        font-size: 15px;
+        font-family: Microsoft YaHei-Bold, Microsoft YaHei;
+        font-weight: bold;
+        color: #000000;
+    }
+
+    .left_2 div:nth-child(2) {
+        margin-top: 12px;
+        font-size: 12px;
+        font-family: Microsoft YaHei-Regular, Microsoft YaHei;
+        font-weight: 400;
+        color: #7D8082;
+    }
+
+    .right {
+        line-height: 100px;
+        padding-left: 25px;
+    }
+
+    .right span {
+        margin-left: 20px;
+        margin-right: 20px;
+        font-size: 13px;
+        font-family: Microsoft YaHei-Regular, Microsoft YaHei;
+        font-weight: 400;
+        color: #000000;
+    }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,122 +1,141 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Total from "@/components/PatientManage/Total";
+import Recent from "@/components/PatientManage/Recent";
 // import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   // 配置默认路由
-  //   path: "", // 路由地址
-  //   redirect: "/patient_manage", // 重定向
-  // },
+    {
+        // 配置默认路由
+        path: "", // 路由地址
+        redirect: "/patient_manage", // 重定向
+    },
 
-  {
-    path: "/home_page",
-    name: "HomePage",
-    component: () =>
-      import(/* webpackChunkName: "HomePage" */ "../pages/HomePage.vue"),
-  },
-  {
-    path: "/appointment",
-    name: "Appointment",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Appointment" */ "../pages/Appointment.vue"),
-  },
+    {
+        path: "/home_page",
+        name: "HomePage",
+        component: () =>
+            import(/* webpackChunkName: "HomePage" */ "../pages/HomePage.vue"),
+    },
+    {
+        path: "/appointment",
+        name: "Appointment",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "Appointment" */ "../pages/Appointment.vue"),
+    },
 
-  {
-    path: "/patient_manage",
-    name: "PatinentManage",
-    component: () =>
-      import(
-        /* webpackChunkName: "PatientManage" */ "../pages/PatientManage.vue"
-      ),
-  },
-  {
-    path: "/iot",
-    name: "Iot",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "Iot" */ "../pages/Iot.vue"),
-  },
-  {
-    path: "/image_center",
-    name: "ImageCenter",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "ImageCenter" */ "../pages/ImageCenter.vue"),
-  },
-  {
-    path: "/statistic",
-    name: "Statistic",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Statistic" */ "../pages/Statistic.vue"),
-  },
-  {
-    path: "/hospital",
-    name: "Hospital",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Hospital" */ "../pages/Hospital.vue"),
-  },
+    {
+        path: "/patient_manage",
+        name: "PatinentManage",
+        component: () =>
+            import(
+                /* webpackChunkName: "PatientManage" */ "../pages/PatientManage.vue"
+                ),
+        children: [
+            {
+                path: "",
+                redirect: "total", // 重定向
+            },
+            {
+                path: "total",
+                // 尤其注意  子路由不能动态加载 否则会报错
+                component: Total
 
-  {
-    path: "/spd",
-    name: "Spd",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "Spd" */ "../pages/Spd.vue"),
-  },
+            },
+            {
+                path: "recent",
+                component: Recent
 
-  {
-    path: "/denture",
-    name: "Denture",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "Denture" */ "../pages/Denture.vue"),
-  },
+            },
+        ]
+    },
+    {
+        path: "/iot",
+        name: "Iot",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "Iot" */ "../pages/Iot.vue"),
+    },
+    {
+        path: "/image_center",
+        name: "ImageCenter",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "ImageCenter" */ "../pages/ImageCenter.vue"),
+    },
+    {
+        path: "/statistic",
+        name: "Statistic",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "Statistic" */ "../pages/Statistic.vue"),
+    },
+    {
+        path: "/hospital",
+        name: "Hospital",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "Hospital" */ "../pages/Hospital.vue"),
+    },
 
-  {
-    path: "/system_log",
-    name: "SystemLog",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "SystemLog" */ "../pages/SystemLog.vue"),
-  },
+    {
+        path: "/spd",
+        name: "Spd",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "Spd" */ "../pages/Spd.vue"),
+    },
 
-  {
-    path: "/operation_log",
-    name: "OperationLog",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(
-        /* webpackChunkName: "OperationLog" */ "../pages/OperationLog.vue"
-      ),
-  },
+    {
+        path: "/denture",
+        name: "Denture",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "Denture" */ "../pages/Denture.vue"),
+    },
+
+    {
+        path: "/system_log",
+        name: "SystemLog",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "SystemLog" */ "../pages/SystemLog.vue"),
+    },
+
+    {
+        path: "/operation_log",
+        name: "OperationLog",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(
+                /* webpackChunkName: "OperationLog" */ "../pages/OperationLog.vue"
+                ),
+    },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
